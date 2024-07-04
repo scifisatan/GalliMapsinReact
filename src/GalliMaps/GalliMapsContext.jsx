4d0631e13d8da9607a37f1762208b8df1c02e95f
scifisatan <brawlershere@gmail.com>
2024-07-05 00:49:43 +0545

    gh-page

--- src/GalliMaps/GalliMapsContext.jsx
+++
@@ -1,6 +0,0 @@
-import { createContext } from 'react';
-
-
-const GalliMapsContext = createContext();
-
-export default GalliMapsContext;
--- src/GalliMaps/useGalliMapsContext.jsx
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-import GalliMapContext from "./GalliMapsContext.jsx";
-export const useGalliMapsContext = () => {
-    const context = useContext(GalliMapContext);
-    if (context === undefined) {
-        throw new Error('useGalliMapContext must be used within a GalliMapProvider');
-    }
-    return context;
-};
